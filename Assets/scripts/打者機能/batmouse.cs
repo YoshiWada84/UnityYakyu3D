5edccfb062524749d544da459156ba7104529b78
YoshiWada84 <h1202219006@hiratagakuen.onmicrosoft.com>
2024-10-11 15:59:51 +0900

    1011_2

---
+++ Assets/scripts/打者機能/batmouse.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batmouse : MonoBehaviour
+{
+    public GameTextes gamas;//試合関連スクリプト
+    public GameObject batbat;
+    //public GameObject ball;
+    [SerializeField, Tooltip("ターゲットオブジェクト")]
+    private GameObject TargetObject;
+
+    [SerializeField, Tooltip("回転軸")]
+    private Vector3 RotateAxis = Vector3.up;
+
+    [SerializeField, Tooltip("速度係数")]
+    private float SpeedFactor = 0.1f;
+    //public float swingATP = 3.0f;//スイングアビリティ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody rigid = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("通常スイングが選択されました。");
+
+            if (TargetObject == null) return;
+
+
+            //バッターを中心に回転する
+            this.transform.RotateAround(
+                TargetObject.transform.position,
+                RotateAxis,
+                360.0f / (1.0f / SpeedFactor) * Time.deltaTime
+                );
+
+        }
+        else if (!Input.GetKey(KeyCode.Space))
+        {
+            if ((-30 > TargetObject.transform.rotation.y) && (TargetObject.transform.rotation.y > -160))
+            {
+                TargetObject.transform.Rotate(0, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (TargetObject == null) return;
+
+            //バッターを中心に回転する
+            this.transform.RotateAround(
+                TargetObject.transform.position,
+                RotateAxis,
+                360.0f / (1.0f / SpeedFactor) * Time.deltaTime
+                );
+
+            Debug.Log("強スイングが選択されました。");
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("The D button is being held down.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+
+    }
+
+}
