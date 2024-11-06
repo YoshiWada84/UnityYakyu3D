d321c294f8b831608e8cde4180eea3002d67e787
YoshiWada84 <h1202219006@hiratagakuen.onmicrosoft.com>
2024-11-06 12:18:50 +0900

    1106

--- Assets/scripts/投手機能/ballcontroll.cs
+++ Assets/scripts/投手機能/ballcontroll.cs
@@ -12,7 +12,12 @@ public class ballcontroll : MonoBehaviour
     public GameTextes gamas;//試合関連スクリプト
     public bool pitch = false;//shootSwitchの弟みたいなやつ
     public static bool shootSwitch;
-   
+
+    public bool strike = false;
+    public bool out1 = false;
+    public bool foul = false;
+    public bool hit = false;
+    public bool homerun = false;
     // Use this for initialization
     void Start()
     {
@@ -24,7 +29,7 @@ public class ballcontroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float z = Random.RandomRange(2.0f, 5.0f);
+        float z = Random.RandomRange(2.0f, 3.0f);
         float timeLimit = Random.RandomRange(0.8f, 1.2f);
         //投球動作
         if (((timer > timeLimit) & shootSwitch) & gamas.gameset == false && gamas.change == false && gamas.Wait == false)
@@ -36,6 +41,12 @@ public class ballcontroll : MonoBehaviour
             timer = 0.0f;
             shootSwitch = false;
             pitch = true;
+            
+            strike = false;
+            out1 = false;
+            foul = false;
+            hit = false;
+            homerun = false;
         }
         else if (((timer > timeLimit * 2) & shootSwitch) & gamas.gameset == false && gamas.change == true && gamas.Wait == false)
         {
@@ -81,7 +92,33 @@ public class ballcontroll : MonoBehaviour
         else if (collision.gameObject.tag == "Strike")
         {
             pitch = false;
+            strike = true;
+        }
+        else if (collision.gameObject.tag == "Out")
+        {
 
+            out1 = true;
+
+
+        }
+        else if (collision.gameObject.tag == "Foul")
+        {
+
+            foul = true;
+
+
+        }
+        else if (collision.gameObject.tag == "Hit")
+        {
+           
+            hit = true;
+            
+
+        }
+        else if (collision.gameObject.tag == "Homerun")
+        {
+
+            homerun = true;
         }
 
 
